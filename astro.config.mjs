963535ef3190a59208556bf2f7ead459db5d022f
Edward <edwarrr@icloud.com>
2026-09-22 14:35:17 +0200

    fix: agregar adaptador de vercel y forzar pnpm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,9 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import vercel from "@astrojs/vercel";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    output: "static", // Cambia a 'server' si en el futuro necesitas Server-Side Rendering (SSR)
+    adapter: vercel(),
+});
